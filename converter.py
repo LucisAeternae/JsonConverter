a51561861af7f741d0245778d99a8d0b0b137f22
LucisAeternae <iampeacemans@gmail.com>
2019-03-24 14:30:58 +0300

    Задание 2, тип тэга теперь зависит от ключа словаря

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import json, sys
 from unittest import TestCase
 
+
 def control():
     data = openjson()
     html = convert(data)
@@ -9,7 +10,7 @@ def control():
 
 def openjson():
     try:
-        with open('source.json') as f:
+        with open('source2.json') as f:
             data = json.load(f)
     except ValueError:
         print('error')
@@ -17,11 +18,10 @@ def openjson():
 
 def convert(data):
     html = ''
-    for tag in data:
-        title = '<h1>%s' % tag['title'] + '</h1>'
-        html += title
-        body = '<p>%s' % tag['body'] + '</p>'
-        html += body
+    for tags in data:
+        for tag in tags:
+            tag = '<%s>%s' % (tag, tags[tag]) + '</%s>' % tag
+            html += tag
     return html
 
 def output(html):
@@ -34,8 +34,13 @@ class TestMainMethod(TestCase):
     def setUp(self):
         self.dict1 = [{'title': 'Title #1', 'body': 'Hello, World 1!'},
                 {'title': 'Title #2', 'body': 'Hello, World 2!'}]
-        self.result1 = '<h1>Title #1</h1><p>Hello, World 1!</p><h1>Title #2' \
-                '</h1><p>Hello, World 2!</p>'
+        self.result1 = '<title>Title #1</title><body>Hello, World 1!</body>' \
+                '<title>Title #2</title><body>Hello, World 2!</body>'
+        self.dict2 = [{'h3': 'Title #1', 'div': 'Hello, World 1!'}]
+        self.result2 = '<h3>Title #1</h3><div>Hello, World 1!</div>'
 
     def test_base_first(self):
         self.assertEqual(convert(self.dict1), self.result1)
+
+    def test_base_second(self):
+        self.assertEqual(convert(self.dict2), self.result2)
