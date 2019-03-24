5a56776bf65dab0150f27246b92f42347fda2741
LucisAeternae <iampeacemans@gmail.com>
2019-03-24 14:42:49 +0300

    Задание 4, возможность работы с вложенными конструкциями, за счет рекурсивной функции которая которая проходит по всем нодам и запускает функцию создания тэга

--- converter.py
+++ converter.py
@@ -4,43 +4,105 @@ from unittest import TestCase
 
 def control():
     data = openjson()
-    html = convert(data)
+    if data:
+        html = convert(data)
+    else:
+        html = "Error"
     output(html)
     return
 
 def openjson():
     try:
-        with open('source2.json') as f:
+        with open('source4.json') as f:
             data = json.load(f)
     except ValueError:
         print('error')
+        data = False
     return data
 
 def convert(data):
     html = ''
     for tags in data:
-        for tag in tags:
-            tag = '<%s>%s' % (tag, tags[tag]) + '</%s>' % tag
-            html += tag
+        block = ''
+        if isinstance(tags, list) or isinstance(tags, dict):
+            for tag in tags:
+                tag = traverse(tag, tags)
+                block +=tag
+            block = '<li>' + block + '</li>'
+        else:
+            block = traverse(tags, data)
+        html += block
+    if isinstance(data, list):
+        html = '<ul>' + html + '</ul>'
     return html
+
+"""Function that create recursion for nested dict"""
+def traverse(data, child_of=False):
+    if isinstance(child_of[data], list):
+        blocks_list = ''
+        for tags in child_of[data]:
+            block = ''
+            for tag_type in tags:
+                tag = traverse(tag_type, tags)
+                block += tag
+            block = '<li>' + block + '</li>'
+            blocks_list += block
+        blocks_list = '<ul>' + blocks_list + '</ul>'
+        blocks_list = '<%s>' % data + blocks_list + '</%s>' % data
+        return blocks_list
+
+    else:
+        tag = createtag(data, child_of)
+        return tag
+
+def createtag(data, child_of):
+    tag = '<%s>%s' % (data, child_of[data]) + '</%s>' % data
+    if isinstance(child_of, list):
+        tag = '<li>' + tag + '</li>'
+    return tag
 
 def output(html):
     sys.stdout.write(html)
 
 if __name__ == '__main__':
+    html = ''
     control()
+
 
 class TestMainMethod(TestCase):
     def setUp(self):
         self.dict1 = [{'title': 'Title #1', 'body': 'Hello, World 1!'},
                 {'title': 'Title #2', 'body': 'Hello, World 2!'}]
-        self.result1 = '<title>Title #1</title><body>Hello, World 1!</body>' \
-                '<title>Title #2</title><body>Hello, World 2!</body>'
+        self.result1 = '<ul><li><title>Title #1</title><body>Hello, World 1!' \
+                '</body></li><li><title>Title #2</title><body>Hello, World 2!' \
+                '</body></li></ul>'
         self.dict2 = [{'h3': 'Title #1', 'div': 'Hello, World 1!'}]
-        self.result2 = '<h3>Title #1</h3><div>Hello, World 1!</div>'
+        self.result2 = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div>' \
+                    '</li></ul>'
+        self.dict3 = [{'h3': 'Title #1', 'div': 'Hello, World 1!'},
+                    {'h3': 'Title #2', 'div': 'Hello, World 2!'}]
+        self.result3 = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div>' \
+                '</li><li><h3>Title #2</h3><div>Hello, World 2!</div></li></ul>'
+        self.dict4 = [{'span': 'Title #1',
+                'content': [{'p': 'Example 1', 'header': 'header 1'}]},
+                {'div': 'div 1'}]
+        self.result4 = '<ul><li><span>Title #1</span><content><ul><li><p>' \
+            'Example 1</p><header>header 1</header></li></ul></content></li>' \
+            '<li><div>div 1</div></li></ul>'
+        self.dict5 = {'p': 'Hello'}
+        self.result5 = '<p>Hello</p>'
 
     def test_base_first(self):
         self.assertEqual(convert(self.dict1), self.result1)
 
     def test_base_second(self):
         self.assertEqual(convert(self.dict2), self.result2)
+
+    def test_base_third(self):
+        self.assertEqual(convert(self.dict3), self.result3)
+
+    def test_base_fourth(self):
+        self.assertEqual(convert(self.dict4), self.result4)
+
+    def test_base_fifth(self):
+        self.assertEqual(convert(self.dict5), self.result5)
